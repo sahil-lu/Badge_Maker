942ee92599e8fdf29d5ae85e25e35d1338aad619
sahils1201 <2023.sahils@isu.ac.in>
2025-09-22 11:52:25 +0530

    Add Vercel configuration files to fix deployment

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
